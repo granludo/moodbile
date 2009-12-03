7fa49500aab301d7b1f0a953334414d2f2c593c7
index02 <index02@gmail.com>
2009-12-03 14:24:38 +0100

    update Moodbile Code

--- moodbile/dummie/data.dum.php
+++
@@ -1,8 +0,0 @@
-<? 
-global $CFG;
-$CFG['sitename'] = "Moodbile";
-$CFG['basepath'] = dirname(__DIR__);
-$CFG['wwwpath'] = "";
-$CFG['active_modules'] = array('courses', 'resources', 'forum', 'events', 'grade', 'user');
-$CFG['theme'] = "moodbile";
-$CFG['default_page'] = "resources"; //pagina por defecto una vez das click al curso deseado
--- moodbile/themes/moodbile/js/testclient_files/moodbile.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('nav li a').live('click', function(){
-        $('nav').find('.active').removeClass('active');
-        $(this).parent().addClass('active');
-        
-        
-    });
-});
